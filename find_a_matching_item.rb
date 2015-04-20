600bec510d2453ca54688114face6aafd0bf0373
Bryan Finlayson <bdfinlayson@gmail.com>
2015-04-20 14:14:55 -0500

    add new kata

---
+++ find_a_matching_item.rb
@@ -0,0 +1,20 @@
+#Enumerable Magic #7 - Find a Matching Item
+#
+#Create a method find that accepts a list and a block, and returns the first item for which the block returns true. If no item in the list matches, return nil.
+#
+#  If you need help, here's a reference:
+#
+#  http://www.rubycuts.com/enum-detect
+
+
+#My solution
+def find list, &block
+  # your solution here
+end
+
+
+#My Tests
+describe "find" do
+  list = [0,1,2,3,5,8,13]
+  Test.assert_equals(find(list){|item| item.odd?}, 1, "1 is the first item in the list that is odd")
+end
